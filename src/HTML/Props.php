52c016a60d8014866157f517283fc21ca326cda7
Nikita Nitichevski <nikita.n@nititech.de>
2025-11-13 09:11:23 +0100

    improved props forwarding function

--- src/HTML/Props.php
+++ src/HTML/Props.php
@@ -9,9 +9,33 @@ class Props {
 		bool $return = false,
 	) {
 		$out = [];
+		$skip = array_map(
+			fn($name) => substr($name, 1),
+			array_filter(
+				$prop_names,
+				fn($name) => str_starts_with($name, '!'),
+			),
+		);
+		$prop_names =  array_filter(
+			$prop_names,
+			fn($name) => !str_starts_with($name, '!'),
+		);
+
+		if ($prop_names[0] === '*') {
+			array_shift($prop_names);
+			$prop_names = [
+				...array_keys($class->__props__),
+				...$prop_names,
+			];
+		}
+
+		$prop_names = array_diff($prop_names, $skip);
 
 		foreach ($prop_names as $name) {
-			if (key_exists($name, $class->__props__)) {
+			if (
+				$name !== 'children' &&
+				key_exists($name, $class->__props__)
+			) {
 				$out[] = $name . '="' . $class->__props__[$name] . '"';
 			}
 		}
